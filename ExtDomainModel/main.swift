cc6797968d4f18ba485f9146afcc5362302ccc48
emh <emh>
2016-10-16 17:08:26 -0700

    Initial Commit

---
+++ ExtDomainModel/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ExtDomainModel
+//
+//  Created by studentuser on 10/16/16.
+//  Copyright © 2016 emh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
